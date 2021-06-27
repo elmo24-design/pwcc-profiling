47e6d2533a0799bfa43efae8ebf47bf9e0fd7ae8
Elmo Mahupil <mahupil24@gmail.com>
2021-06-27 21:11:12 +0800

    2nd commit
    just tested github desktop

--- src/views/Signin.js
+++ src/views/Signin.js
@@ -107,7 +107,7 @@ const Signin = () => {
                <img src={logo} alt="logo" className="logo" />
             </div>
             <form noValidate autoComplete="off" onSubmit={handleSubmit} className="form">
-               <h1>PWCC Profiling App</h1>
+               <h1>PWCC Profiling Application</h1>
                <TextField
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
